5804ee97ad0c21ee8abc2f7f21b4c0111c9b9bf2
fenn7 <afenn666@gmail.com>
2022-06-26 00:03:30 +0100

    Initial commit

---
+++ src/main/java/net/fenn7/thatchermod/ThatcherMod.java
@@ -0,0 +1,19 @@
+package net.fenn7.thatchermod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ThatcherMod implements ModInitializer {
+	public static final String MOD_ID = "thatchermod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/fenn7/thatchermod/ThatcherModClient.java
@@ -0,0 +1,11 @@
+package net.fenn7.thatchermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThatcherModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
