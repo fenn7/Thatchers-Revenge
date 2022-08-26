2ac2abb578b62e96bf50811e16a4ee1ba8b5a81e
fenn7 <afenn666@gmail.com>
2022-08-26 18:20:16 +0100

    paramilitary entity done

--- src/main/java/net/fenn7/thatchermod/entity/client/ParamilitaryModelRenderer.java
+++ src/main/java/net/fenn7/thatchermod/entity/client/ParamilitaryModelRenderer.java
@@ -66,8 +66,8 @@ public class ParamilitaryModelRenderer extends GeoEntityRenderer<ParamilitaryEnt
             stack.push();
             stack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(bone.getRotationX()));
             stack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(bone.getRotationY()));
-            stack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(bone.getRotationZ() - 90));
-            stack.translate(-1.25D, 0.0D, 0.15D);
+            stack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(bone.getRotationZ() - 180));
+            stack.translate(0.0D, -1.25D, 0.15D);
             stack.scale(0.7f, 0.7f, 0.7f);
             MinecraftClient.getInstance().getItemRenderer().renderItem(mainHand, ModelTransformation.Mode.NONE,
                     packedLightIn, packedOverlayIn, stack, this.rtb, 0);
